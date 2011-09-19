55dc5d7be8b90881e5f5611b66b45510340fe669
Kwinten Missiaen <kwinten.missiaen@student.kuleuven.be>
2011-09-19 17:20:44 +0200

    Updated hello script so that error messages are more precise. They are now given when the table does not exist, or when the table does exist but has a player with the same name

--- hello.php
+++ hello.php
@@ -15,25 +15,49 @@
 
     $reply1JSON = json_decode($reply1, true);
 
-    if($reply1JSON['result'] != NULL)
-    {
-	//Table exists
-	$tableName = $playerName."_".$classTable."_test";
-	$nbPlayers = 3;
-
-	// This sets up the test table. If the test table already exists, nothing will happen.
-	setUpTableRequest($tableName, $nbPlayers);
-	
-	$ackType = "Acknowledge";
-	$ackMessage = "Hello acknowledged!";
-	$ackArray = array("type" => $ackType,
-	    "message" => $ackMessage,
-	    "testTable" => $tableName);
-    }
-    else 
+    if($reply1JSON['type'] == 'error')
     {
 	$ackArray = array("type" => "InvalidInput",
 	    "message" => "Table '".$classTable."' does not exist.");	
+    }
+    else if($reply1JSON['result'] != null)
+    {
+
+	//Table exists
+
+	//If the player already exists, deny permission
+
+	$exists = false;
+
+	for($i=0; $i< count($reply1JSON['result']['player']); $i++)
+	{
+
+		if($reply1JSON['result']['player'][$i]['name'] == $playerName)
+		{
+			$exists = true;
+		}
+	}
+
+	if($exists)
+	{
+		$ackArray = array("type" => "InvalidInput",
+			"message" => "A player with name ".$playerName." is already playing at table ".$classTable);
+	}
+	else
+	{
+		//Hello is valid. Set up test table and reply with ack.
+		$tableName = $playerName."_".$classTable."_test";
+		$nbPlayers = 3;
+
+		// This sets up the test table. If the test table already exists, nothing will happen.
+		setUpTableRequest($tableName, $nbPlayers);
+	
+		$ackType = "Acknowledge";
+		$ackMessage = "Hello acknowledged!";
+		$ackArray = array("type" => $ackType,
+		    "message" => $ackMessage,
+		    "testTable" => $tableName);
+    	}
     }
 
     $ackJSON = json_encode($ackArray);
