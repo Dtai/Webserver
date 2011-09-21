551eb49a28445cbdbb9113931c2e6d72ea979c9f
Kwinten Missiaen <kwinten.missiaen@student.kuleuven.be>
2011-09-21 11:26:29 +0200

    goodbye message now deletes testTable

--- goodbye.php
+++ goodbye.php
@@ -1,11 +1,18 @@
 <?php
 	require("misc.php");
+	$classTable = $_REQUEST['tableName'];
+	$playerName = $_REQUEST['playerName'];
 
 	$arr = array('type' => "leaveTable", 
-	"tableName" => $_REQUEST['tableName'],
-	"playerName" => $_REQUEST['playerName']
+	"tableName" => $classTable,
+	"playerName" => $playerName
 	);
 
 	$reply = sendRequest($arr);
+
+	$testTableName = $playerName."_".$classTable."_test";
+	$arr2 = array("type" => "killTestTable", "tableName" => $testTableName);
+	$reply2 = sendRequest($arr2);
+
 	echo($reply);
 ?>
