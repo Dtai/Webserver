dddc6de7edfe52062ab3b19d5914ab18cac6cd20
Kwinten Missiaen <kwinten.missiaen@student.kuleuven.be>
2011-09-23 10:55:33 +0200

    Updated index page, tutorial and download link according to feedback.

---
+++ download.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<title> Download RuleCreator</title>
+		<link href="style.css" type="text/css" rel="stylesheet"/>
+	</head>
+	
+	<body style="width: 60%; margin: auto;">
+		<h1> Download RuleCreator </h1>
+		<p align="justify">
+			Om RuleCreator te gebruiken moet je enkel een installer downloaden. Wanneer deze installer uitgevoerd wordt, wordt automatisch RuleCreator ge&#239;nstalleerd, er zijn geen admin rechten nodig.
+			Tijdens de installatie heb je de mogelijkheid om een installeerlocatie te kiezen. Als de installatie gedaan is, staat er een snelkoppeling op het bureaublad.
+		</p>
+		
+		Voor verdere uitleg kan je de documentatie lezen. Om RuleCreator ten volle gebruiken moet je ook een tafel aanmaken.
+		<div class="input">
+			<a class="Metrobutton red" href="installer/rc.msi">Download RuleCreator voor Windows</a>
+			<a class="Metrobutton" href="documentation/indexWeb.html">RuleCreator Documentatie</a>
+			<a class="Metrobutton cyan" href="server/makeTableForm.php">Nieuwe tafel aanmaken</a>
+		</div>
+	<body>
+</html>
