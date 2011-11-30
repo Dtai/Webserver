2535995d34904c88112c9834175acb028aac576c
Tias Guns <tias.guns@cs.kuleuven.be>
2011-11-30 18:38:35 +0100

    Make  a configurable option

--- server/misc.php
+++ server/misc.php
@@ -1,15 +1,16 @@
 <?php
 
+$PokerbotServer = "localhost";
 
 function sendRequest($array)
 {
+    global $PokerbotServer;
 
     $arrReq = array('request' => $array);
     $message = json_encode($arrReq);
 
     $socket = socket_create(AF_INET, SOCK_STREAM, 0);
-    socket_connect($socket, "borgraf", 20000);
-    //socket_connect($socket1, "joske", 20000);
+    socket_connect($socket, $PokerbotServer, 20000);
     socket_write($socket, $message);
     socket_shutdown($socket,1); 
       
