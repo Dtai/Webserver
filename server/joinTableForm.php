c843b501e2b72020117b410d758ccee3f3f1458d
Tias Guns <tias.guns@cs.kuleuven.be>
2011-12-01 14:45:28 +0100

    Document libraries and debug function

--- server/joinTableForm.php
+++ server/joinTableForm.php
@@ -3,7 +3,7 @@
 <form action ="joinTable.php" method ="post">
 	<p> Naam van de tafel: <input type="text" name="tableName" /> </p>
 	<p> Naam van de speler: <input type="text" name="playerName"/> </p>
-	<p> Beschrijving (prolog) <input type="text" name ="description"/> (callbot: <i>do(call, 1) :- true.)</i></p>
+	<p> Beschrijving (prolog) <input type="text" name ="description" value="do(call, 1) :- true." /> </p>
 	<p><input type ="submit" /> </p>
 </form>
 
