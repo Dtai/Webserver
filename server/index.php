672b9394fd58e6ec625aa50e66808dcaff37a2b0
Tias Guns <tias.guns@cs.kuleuven.be>
2011-12-01 12:02:49 +0100

    Clean up 'tafel beheer' page, some styling changes

--- server/index.php
+++ server/index.php
@@ -6,17 +6,35 @@
 	</head>
 	
 	<body>
-		<h1>Welkom op de website van de pokerbotserver!</h1>
+		<h1>De Pokerbot tafels beheren</h1>
 		
 		 <div class="input">			
-			<a class="Metrobutton red" href="makeTableForm.php">Nieuwe tafel</a>
-			<a class="Metrobutton" href="joinTableForm.php">Spelen op een tafel</a>
-			<a class="Metrobutton blue" href="ObserveTableForm.php">Grafiek tafel</a>
-			<a class="Metrobutton green" href="stopTableForm.php">Pauzeer tafel</a><br/>
-			<a class="Metrobutton cyan" href="resumeTableForm.php">Hervat tafel</a>
-			<a class="Metrobutton yellow" href="killTableForm.php">Verwijder tafel</a>
-			<a class="Metrobutton pink" href="listTables.php">Toon tafels</a> 
-			<a class="Metrobutton" href="kickPlayerForm.php">Kick speler</a>
+			<a class="Metrobutton cyan" href="makeTableForm.php">Nieuwe tafel</a>
+			<a class="Metrobutton green" href="ObserveTableForm.php">Toon tafel</a>
+			<a class="Metrobutton pink" href="listTables.php">Toon alle tafels</a>
 		 </div>
+         <p>&nbsp;</p>
+		 <div class="input">			
+            <h3>Tafelbeheer (wachtwoord nodig)</h3>
+			<a class="Metrobutton" href="stopTableForm.php">Pauzeer tafel</a>
+			<a class="Metrobutton yellow" href="resumeTableForm.php">Hervat tafel</a>
+			<a class="Metrobutton blue" href="kickPlayerForm.php">Kick speler van tafel</a>
+			<a class="Metrobutton red" href="killTableForm.php">Verwijder tafel</a>
+		 </div>
+
+<?php 
+    if (array_key_exists('debug', $_GET)) {
+         ?>
+         <p>&nbsp;</p>
+         <div class="input">
+            <h3>Debugging:</h3>
+			<a class="Metrobutton" href="joinTableForm.php">Spelen op een tafel</a>
+        </div>
+        <?php
+    }
+?>
+<p>
+Terug naar de <a href="../">hoofdpagina</a>.
+</p>
 	</body>
 </html>
