e4c30d33b160cf62ffd1cdcdebc267e9fbf228e0
Tias Guns <tias@ulyssis.org>
2016-08-02 15:48:33 +0200

    goodbye: no more testtable

--- server/goodbye.php
+++ server/goodbye.php
@@ -10,9 +10,14 @@
 
 	$reply = sendRequest($arr);
 
+    /**
+     * We no longer create testtables
+     * Should somebody wish to re-active this,
+     * then make the test-table in the client!
 	$testTableName = $playerName."_".$classTable."_test";
 	$arr2 = array("type" => "killTestTable", "tableName" => $testTableName);
 	$reply2 = sendRequest($arr2);
+    */
 
 	echo($reply);
 ?>
